4d08c1b11bd9a304087b6705301599ed3a327f39
LidiaBaciu <lidia.baciu97@gmail.com>
2020-09-30 16:06:17 +0200

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,18 @@ import { Link } from "gatsby"
 import SEO from "../components/SEO"
 
 const Error = () => {
-  return <h2>error page</h2>
+  return (
+    <Layout>
+      <main className="error-page">
+        <div className="error-container">
+          <h1>Oops! The page you are looking for doesn't exist!</h1>
+          <Link to="/" className="btn">
+            Home
+          </Link>
+        </div>
+      </main>
+    </Layout>
+  )
 }
 
 export default Error
