cfb2f37a79eb78d88de7246703c9a47a8cff0f61
LidiaBaciu <lidia.baciu97@gmail.com>
2020-10-01 12:14:01 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: `https://strapi-gatsby-portofolio.herokuapp.com`,
         queryLimit: 1000, // Default to 100
         contentTypes: [`jobs`, `projects`, `blogs`],
         singleTypes: [`about`],
